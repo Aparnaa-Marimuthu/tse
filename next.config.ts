5e000d1d0af535b067e599f0e1926b32601e98e3
Aparnaa-Marimuthu <aparnaa.marimuthu@7dxperts.com>
2026-05-06 16:57:33 +0530

    Update next js other sttufs for build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-import type { NextConfig } from "next";  
-  
-const nextConfig: NextConfig = {  
-  eslint: {  
-    ignoreDuringBuilds: true,  
-  },  
-};  
-  
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {};
+
 export default nextConfig;
